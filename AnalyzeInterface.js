f552edb2a06910f3df341eb91f526752e580036e
xtxk110 <329986833@qq.com>
2019-01-10 09:01:43 +0800

    TV app所需JS文件

---
+++ AnalyzeInterface.js
@@ -0,0 +1,15 @@
+﻿//获取接口数据
+function GetInterfaceData(){
+	var data=[        "https://jx.618g.com/?url=",   
+			  "https://vip.mpos.ren/v/?url=",   
+                          "http://jx.52xftv.cn/?url=",
+                          "https://cdn.yangju.vip/k/?url=",
+                          "https://jx.ab33.top/vip/?url=",
+                          "http://api.baiyug.cn/vip/?url=",                         
+                          "http://jqaaa.com/jx.php?url=",
+			  "http://api.wlzhan.com/sudu/?url=",
+                          "http://jx.ovov.cc/?url="
+	];
+	return data;
+}
+
---
+++ Web.js
@@ -0,0 +1,20 @@
+﻿//获取影视网站数据
+function GetWebUrl(){
+	var data=[{name:"爱奇艺影视",url:"http://m.iqiyi.com/"},
+                  {name:"腾讯视频",url:"https://m.v.qq.com/"},
+                  {name:"优酷视频",url:"http://www.youku.com/"},
+                  {name:"搜狐视频",url:"https://m.tv.sohu.com/"},
+                  {name:"乐视视频",url:"https://m.le.com/"},
+                  {name:"酷哥电影",url:"http://www.kgvod.com/#"},
+                  {name:"发烧屋影视",url:"http://www.hifiwu.com/#"},
+                  {name:"19影视",url:"http://www.vod19.com/#"},
+                  {name:"全民影院",url:"http://www.633tv.com/#"},
+                  {name:"海兔影院",url:"http://www.haitum.com/#"}
+	         ];
+	return data;
+}
+
+var obj={
+         ver:3.2,
+	 url:"http://www.gotopsports.com/Content/TV.apk"
+	};
