4e18a3f472d89228088f49809fbbdf65d6f21e3a
xtxk110 <329986833@qq.com>
2019-01-15 09:11:34 +0800

    更新接口数据

--- AnalyzeInterface.js
+++ AnalyzeInterface.js
@@ -1,14 +1,14 @@
 ﻿//获取接口数据
 function GetInterfaceData(){
-	var data=[        "https://jx.618g.com/?url=",   
-			  "https://vip.mpos.ren/v/?url=",   
-                          "http://jx.52xftv.cn/?url=",
-                          "https://cdn.yangju.vip/k/?url=",
-                          "https://jx.ab33.top/vip/?url=",
-                          "http://api.baiyug.cn/vip/?url=",                         
-                          "http://jqaaa.com/jx.php?url=",
-                          "http://jx.ovov.cc/?url="
-	];
+	var data=[          "https://jx.618g.com/?url=", 
+						"https://cdn.yangju.vip/k/?url=",
+						"http://jx.ovov.cc/?url=",
+						"http://jqaaa.com/jx.php?url=",
+						"http://api.baiyug.cn/vip/?url=",    
+						"https://vip.mpos.ren/v/?url=",   
+                        "https://jx.ab33.top/vip/?url=" ,
+						"http://jx.52xftv.cn/?url="
+			];
 	return data;
 }
 
