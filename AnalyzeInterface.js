73082ac17599460bf49e0d9d8a16a85449b3c4d3
xtxk110 <329986833@qq.com>
2019-01-16 10:05:10 +0800

    更新解析接口

--- AnalyzeInterface.js
+++ AnalyzeInterface.js
@@ -1,13 +1,14 @@
 ﻿//获取接口数据
 function GetInterfaceData(){
 	var data=[          "https://jx.618g.com/?url=", 
+						"http://api.smq1.com/?url=",
+						"http://api.51ckm.com/Box.php?url=",
+						"http://api.zuilingxian.com/jiexi.php?url=",
+						"https://jx.km58.top/jx/?url=",
 						"https://cdn.yangju.vip/k/?url=",
 						"http://jx.ovov.cc/?url=",
 						"http://jqaaa.com/jx.php?url=",
-						"http://api.baiyug.cn/vip/?url=",    
-						"https://vip.mpos.ren/v/?url=",   
-                        "https://jx.ab33.top/vip/?url=" ,
-						"http://jx.52xftv.cn/?url="
+						"http://api.baiyug.cn/vip/?url="   
 			];
 	return data;
 }
