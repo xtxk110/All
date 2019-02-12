098f1a6ca1c57c1b26a88b1aea0143d0f26b1521
xtxk110 <329986833@qq.com>
2019-02-12 10:03:49 +0800

    s

--- Web.js
+++ Web.js
@@ -5,11 +5,9 @@ function GetWebUrl(){
                   {name:"优酷视频",url:"http://www.youku.com/"},
                   {name:"搜狐视频",url:"https://m.tv.sohu.com/"},
                   {name:"乐视视频",url:"https://m.le.com/"},
-                  {name:"酷哥电影",url:"http://www.kgvod.com/#"},
-                  {name:"发烧屋影视",url:"http://www.hifiwu.com/#"},
-                  {name:"19影视",url:"http://www.vod19.com/#"},
-                  {name:"全民影院",url:"http://www.633tv.com/#"},
-                  {name:"海兔影院",url:"http://www.haitum.com/#"}
+                  {name:"365电影",url:"https://www.365dy.cn/#"},
+                  {name:"88影视",url:"https://m.88ys.cn/#"},
+                  {name:"全民影院",url:"http://www.633tv.com/#"}
 	         ];
 	return data;
 }
